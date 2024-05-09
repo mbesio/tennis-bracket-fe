51f2bc3aff6fb29d73b50a5e247769faa23d14c7
Matteo Besio <matteo.besio@gmail.com>
2024-05-08 21:42:00 -0400

    linked production server

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
-// export const SERVER_DOMAIN = 'https://tennis-bracket-be.onrender.com/api'
+export const SERVER_DOMAIN = 'https://tennis-bracket-be.onrender.com/api'
 
-export const SERVER_DOMAIN = 'http://localhost:3001/api'
+// export const SERVER_DOMAIN = 'http://localhost:3001/api'
 
 export const predictionFirstQuarterSemiFinalist =
   'predictionFirstQuarterSemiFinalist'
