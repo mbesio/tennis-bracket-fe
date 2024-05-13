26e787ab1ead091c40b44c632ae4edf99a70c8e9
Matteo Besio <matteo.besio@gmail.com>
2024-05-13 14:29:01 -0400

    nit

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-// export const SERVER_DOMAIN = 'https://tennis-bracket-be.onrender.com/api'
+// export const SERVER_DOMAIN = '/api' // FOR PRODUCTION
 
 export const SERVER_DOMAIN = 'http://localhost:3001/api'
 
